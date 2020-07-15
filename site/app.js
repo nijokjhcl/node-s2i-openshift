e5d81c075a803925b4a873ceee50e20fc7935e53
nijokjhcl <68075441+nijokjhcl@users.noreply.github.com>
2020-07-15 16:36:08 +0530

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,7 +52,7 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Patient 2",
+        "name": "Patient 3",
         "age": 38,
         "gender": "male",
         "street": "34 Main Street",
